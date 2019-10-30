b4cb7295ef3b0bf286f160d43cfd06e4b6dfe0a8
s0ul <s0ul@protonmail.com>
2019-10-30 12:10:43 +0700

    Added Rate model and it's admin

---
+++ server/apps/rate/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+CURRENCY_CHOICE = (
+    ('CZK', 'Czech Republic Koruna'),
+    ('EUR', 'Euro'),
+    ('PLN', 'Polish Zloty'),
+    ('USD', 'United States Dollar')
+)
+
+
+class Rate(models.Model):
+    """Model for storing rate of currencies
+
+    Fields:
+        from_cur (str): input currency for conversation
+        to_cur (str): result currency for conversation
+        rate (float): current rate of `from_cur` currency
+            relatively to `to_cur`
+        updated_at (datetime): date and time of last rate update
+    """
+    from_cur = models.CharField(
+        max_length=255,
+        verbose_name='Currency for conversation',
+        choices=CURRENCY_CHOICE
+    )
+
+    to_cur = models.CharField(
+        max_length=255,
+        verbose_name='Result currency',
+        choices=CURRENCY_CHOICE
+    )
+
+    rate = models.DecimalField(
+        max_digits=19,
+        decimal_places=12,
+        verbose_name='Current rate'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Last update'
+    )
--- server/manage.py
+++ server/manage.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
