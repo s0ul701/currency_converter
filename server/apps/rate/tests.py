6d856b496bf06cb85b1c51208f80e83652918304
s0ul <s0ul@protonmail.com>
2019-11-04 15:17:37 +0700

    Added corrects based on CR
    Added test for celery task `rates_update`

--- server/apps/rate/tests.py
+++ server/apps/rate/tests.py
@@ -1,7 +1,10 @@
-from rest_framework.test import APITestCase
-
-from apps.rate.models import Rate, CURRENCY_CHOICE
+from apps.rate.models import Rate, Currencies
 from apps.rate.serializers import RateSerializer
+from apps.rate.tasks import update_rates
+
+from django.test import TestCase
+
+from rest_framework.test import APITestCase
 
 
 class RateAPITestCase(APITestCase):
@@ -9,8 +12,8 @@ class RateAPITestCase(APITestCase):
 
     def test_get_all_initial_rates(self):
         """Test if initial data migration for rates creates all rates"""
-        for (from_cur, _) in CURRENCY_CHOICE:
-            for (to_cur, _) in CURRENCY_CHOICE:
+        for (from_cur, _) in Currencies.CHOICES:
+            for (to_cur, _) in Currencies.CHOICES:
                 if from_cur != to_cur:
                     self.assertEqual(
                         Rate.objects.filter(from_cur=from_cur, to_cur=to_cur).count(),
@@ -29,3 +32,17 @@ class RateAPITestCase(APITestCase):
             response_serializer.initial_data,
             RateSerializer(rates, many=True).data
         )
+
+
+class RatesTaksTestCase(TestCase):
+    """Class for testing Rate`s app tasks"""
+    def test_rates_update_task(self):
+        """Test if rates before update and after update have different date modification"""
+        last_update_dates = {
+            rate.id: rate.updated_at for rate in Rate.objects.all()}
+        update_rates()
+        for (rate_id, rate_update) in last_update_dates.items():
+            self.assertNotEqual(
+                Rate.objects.get(id=rate_id).updated_at,
+                rate_update
+            )
