bccda797eaeecf83db10027594ffa1df19861f81
s0ul <s0ul@protonmail.com>
2019-11-05 14:41:26 +0700

    Deleted redundant migration

--- server/config/migrations/rate/0001_initial.py
+++ server/config/migrations/rate/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('from_cur', models.CharField(choices=[('CZK', 'Czech Republic Koruna'), ('EUR', 'Euro'), ('PLN', 'Polish Zloty'), ('USD', 'United States Dollar')], max_length=3, verbose_name='Currency for conversation')),
                 ('to_cur', models.CharField(choices=[('CZK', 'Czech Republic Koruna'), ('EUR', 'Euro'), ('PLN', 'Polish Zloty'), ('USD', 'United States Dollar')], max_length=3, verbose_name='Result currency')),
-                ('rate', models.DecimalField(decimal_places=12, default=None, max_digits=19, null=True, verbose_name='Current rate')),
+                ('rate', models.DecimalField(decimal_places=12, max_digits=19, null=True, verbose_name='Current rate')),
                 ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Last update')),
             ],
             options={
